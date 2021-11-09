2586037d9f84b8ec17257098c6ae83e59e94c934
soncheung2 <93186512+soncheung2@users.noreply.github.com>
2021-11-09 17:03:19 +0800

    Add files via upload

---
+++ printBig7(1).py
@@ -0,0 +1,15 @@
+'''
+Created on Nov 9, 2021
+
+@author: sonia cheung
+'''
+>>> print('''
+77777777777777
+            7
+           7
+          7
+         7
+        7
+       7
+      7
+      ''')
